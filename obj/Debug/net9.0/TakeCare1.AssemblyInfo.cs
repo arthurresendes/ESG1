58856adb4da8adc07cb50a3428ca0c26849e3f43
correia23davi <davi.correia880fe@gmail.com>
2025-06-03 16:22:23 -0300

    att

--- obj/Debug/net9.0/TakeCare1.AssemblyInfo.cs
+++ obj/Debug/net9.0/TakeCare1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TakeCare1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ab4306ce4a957c4359e38cdfd1d73ff0307c388")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bfc230040aa37902e884829a6749289667556d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TakeCare1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TakeCare1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
