827f055ca99812fcbddb93b186a5e600b549996d
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-06-24 21:35:29 -0300

    Atualização

--- obj/Debug/net9.0/TakeCare1.AssemblyInfo.cs
+++ obj/Debug/net9.0/TakeCare1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TakeCare1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d192be7735754a6cfe217053bb9373ee9ab8304f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbf198e3dfc5522d7b2ff2bd4cca1cd363dbb91c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TakeCare1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TakeCare1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
